fdc87c537fae813016ac7db7ba4c27f6279a67a3
Robert Snyder <r.snyder49@gmail.com>
2018-09-17 19:18:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |info| 
-      student self.new(info)
+      self.new(info)
     end
     
     student
